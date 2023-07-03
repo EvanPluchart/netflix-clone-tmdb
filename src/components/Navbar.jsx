71bc37a2e7b6283b68933f0b64b73a05969b8ee2
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-03 03:10:57 +0200

    feature: Création du composant Navbar.
    * Ce composant est pour le moment uniquement visuel.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import reactLogo from '../assets/react.svg'
+import netflixLogo from '../assets/netflix-logo.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBell, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-black px-14 py-5 flex items-center justify-between">
+
+            <div className={'flex space-x-12 items-center'}>
+                <div className="flex items-center">
+                    <img src={netflixLogo} alt="Netflix logo" className="h-8" />
+                </div>
+
+                <ul className="hidden md:flex space-x-6">
+                    <li>
+                        <a href="#" className="text-white hover:text-gray-300">
+                            Accueil
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-white hover:text-gray-300">
+                            Séries
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-white hover:text-gray-300">
+                            Films
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="flex items-center space-x-6">
+                <FontAwesomeIcon
+                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                    icon={faMagnifyingGlass}
+                />
+
+                <FontAwesomeIcon
+                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                    icon={faBell}
+                />
+
+                <img
+                    src={reactLogo}
+                    alt="Profile"
+                    className="h-8 w-8 rounded-full object-cover"
+                />
+            </div>
+        </nav>
+    );
+}
