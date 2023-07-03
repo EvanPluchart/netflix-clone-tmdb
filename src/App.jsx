50bb4ac9031c336435a8ffdd6a018579e1a90321
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 00:12:12 +0200

    feature: Utilisation du footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
     return (
         <>
             <Navbar />
+            <Footer />
         </>
     )
 }
