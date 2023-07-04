eefba5687b5367524612ab2e83b0f9b33f0d29f2
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 15:18:48 +0200

    feature: Mise en forme de la liste des films.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -20,8 +20,8 @@ export default function MovieList() {
 
     return (
         <>
-            <div className="bg-zinc-900 px-6 md:px-14 py-6 flex items-center justify-between">
-                <div className="movie-list flex space-x-6 flex-wrap w-full">
+            <div className="bg-zinc-900 px-6 md:px-72 py-6 flex">
+                <div className="movie-list flex flex-wrap w-full items-center justify-center md:justify-between gap-12 md:gap-8 md:after:flex-auto md:after:content-['']">
                     {movies.map((movie) => (
                         <MovieCard key={movie.id} movie={movie} />
                     ))}
