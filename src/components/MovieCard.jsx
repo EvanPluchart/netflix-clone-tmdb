8f031b731bc4f27096061d535d28171c2dd40620
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 18:20:42 +0200

    feature: Ajout d'une redirection vers la page détail d'un film.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import {Link} from "wouter";
 
 export default function MovieCard({ movie }) {
     return (
-        <div className={'flex flex-col items-center space-y-3 cursor-pointer'}>
+        <Link className={'flex flex-col items-center space-y-3 cursor-pointer'} href={'/films/' + movie.id}>
             <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
                 className="w-32 rounded-lg shadow-lg"
             />
             <h3 className={'w-32 text-white font-bold overflow-hidden text-ellipsis whitespace-nowrap'}>{movie.title}</h3>
-        </div>
+        </Link>
     );
 }
