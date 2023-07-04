818837e981e1ef35c3f14f7d20cf915dd0602b1d
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 02:19:57 +0200

    feature: Affichage des films sur la page pricipale.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import MovieList from "./components/MovieList.jsx";
 
 export default function App() {
     return (
-        <>
+        <main className='bg-zinc-900'>
             <Navbar />
+            <MovieList />
             <Footer />
-        </>
+        </main>
     )
 }
