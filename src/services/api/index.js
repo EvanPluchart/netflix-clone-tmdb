cf0a51ff312edacbc8ea44ec82b9503c89ded701
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 18:20:23 +0200

    feature: Ajout d'une fonction récupérant les informations d'un film.

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -8,3 +8,11 @@ export function fetchMovies(pageNumber) {
         .then((data) => data.results)
         .catch((error) => console.error(error));
 }
+
+export function fetchMovieDetails(movieId) {
+    return fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=fr-FR`)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
