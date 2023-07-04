8a14c7941ec3e840c1ac52e55970adf3de777e3a
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 15:44:42 +0200

    fix: Harmonisation du gap entre les films de la liste.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -21,7 +21,7 @@ export default function MovieList() {
     return (
         <>
             <div className="bg-zinc-900 px-6 md:px-72 py-6 flex">
-                <div className="movie-list flex flex-wrap w-full items-center justify-center md:justify-between gap-12 md:gap-8 md:after:flex-auto md:after:content-['']">
+                <div className="movie-list flex flex-wrap w-full items-center justify-center md:justify-normal gap-12 md:gap-8">
                     {movies.map((movie) => (
                         <MovieCard key={movie.id} movie={movie} />
                     ))}
