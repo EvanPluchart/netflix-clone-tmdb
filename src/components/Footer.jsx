b8da1a5a423da71fa3207f7f186c608da80fabad
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-04 15:36:50 +0200

    fix: Déplacement de la liste des réseaux sociaux.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,28 @@ import {faFacebook, faInstagram, faTwitter, faYoutube} from "@fortawesome/free-b
 export default function Footer() {
     return (
         <footer className="hidden md:flex flex-col bg-zinc-900 px-6 md:px-72 py-6 space-y-6 pt-16 items-center">
-            <div className="flex items-start space-x-6">
-                <FontAwesomeIcon
-                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
-                    icon={faFacebook}
-                />
-                <FontAwesomeIcon
-                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
-                    icon={faInstagram}
-                />
-                <FontAwesomeIcon
-                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
-                    icon={faTwitter}
-                />
-                <FontAwesomeIcon
-                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
-                    icon={faYoutube}
-                />
-            </div>
-
             <div className="text-gray-400 text-xs flex items-center space-x-32">
                 <ul className="flex-col space-y-3">
+
+                    <div className="flex items-start space-x-6">
+                        <FontAwesomeIcon
+                            className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                            icon={faFacebook}
+                        />
+                        <FontAwesomeIcon
+                            className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                            icon={faInstagram}
+                        />
+                        <FontAwesomeIcon
+                            className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                            icon={faTwitter}
+                        />
+                        <FontAwesomeIcon
+                            className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                            icon={faYoutube}
+                        />
+                    </div>
+
                     <li>Audiodescription</li>
                     <li>Relations investisseurs</li>
                     <li>Confidentialité</li>
