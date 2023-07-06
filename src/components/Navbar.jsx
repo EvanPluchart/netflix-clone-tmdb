85d49d8c325753b31b933e3f6f30144ad5092837
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-06 14:30:03 +0200

    fix: Ajout de la responsive mobile.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,49 +7,71 @@ import {Link} from "wouter";
 
 export default function Navbar() {
     return (
-        <nav className="bg-zinc-900 px-6 md:px-14 py-6 flex items-center justify-between">
+        <nav className="bg-zinc-900 px-6 md:px-14 py-6 flex flex-col md:flex-row items-center justify-between gap-4 fixed w-full">
 
-            <div className={'flex space-x-12 items-center'}>
-                <Link className="flex items-center" href={'/'}>
-                    <img src={netflixLogo} alt="Netflix logo" className="h-8 cursor-pointer" />
-                </Link>
+            <div className={'flex space-x-12 items-center justify-between w-full'}>
+                <div className={'flex space-x-12 items-center'}>
+                    <Link className="flex items-center" href={'/'}>
+                        <img src={netflixLogo} alt="Netflix logo" className="h-8 cursor-pointer" />
+                    </Link>
 
-                <ul className="hidden md:flex space-x-6">
-                    <li>
-                        <Link href="/" className="text-white hover:text-gray-300">
-                            Accueil
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/series" className="text-white hover:text-gray-300">
-                            Séries
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/films" className="text-white hover:text-gray-300">
-                            Films
-                        </Link>
-                    </li>
-                </ul>
+                    <ul className="hidden md:flex space-x-6">
+                        <li>
+                            <Link href="/" className="text-white hover:text-gray-300">
+                                Accueil
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/series" className="text-white hover:text-gray-300">
+                                Séries
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/films" className="text-white hover:text-gray-300">
+                                Films
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="flex items-center space-x-6">
+                    <FontAwesomeIcon
+                        className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                        icon={faMagnifyingGlass}
+                    />
+
+                    <FontAwesomeIcon
+                        className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
+                        icon={faBell}
+                    />
+
+                    <img
+                        src={reactLogo}
+                        alt="Profile"
+                        className="h-8 w-8 rounded-full object-cover"
+                    />
+                </div>
             </div>
 
-            <div className="flex items-center space-x-6">
-                <FontAwesomeIcon
-                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
-                    icon={faMagnifyingGlass}
-                />
 
-                <FontAwesomeIcon
-                    className='h-6 w-6 text-white hover:text-gray-300 cursor-pointer'
-                    icon={faBell}
-                />
+            <ul className="md:hidden flex space-x-6">
+                <li>
+                    <Link href="/" className="text-white hover:text-gray-300">
+                        Accueil
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/series" className="text-white hover:text-gray-300">
+                        Séries
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/films" className="text-white hover:text-gray-300">
+                        Films
+                    </Link>
+                </li>
+            </ul>
 
-                <img
-                    src={reactLogo}
-                    alt="Profile"
-                    className="h-8 w-8 rounded-full object-cover"
-                />
-            </div>
         </nav>
     );
 }
