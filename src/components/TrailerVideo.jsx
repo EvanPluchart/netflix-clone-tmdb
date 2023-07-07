f8ac9238964df25ba0e2a25fb36e06e79b609e71
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-07 02:59:36 +0200

    fix: Correction de la taille de la vidéo au format desktop.

--- src/components/TrailerVideo.jsx
+++ src/components/TrailerVideo.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function TrailerVideo({ videos }) {
     return (
-        <div>
+        <div className={'w-full'}>
             {videos === undefined || videos.results === undefined || videos.results.length === 0 ? <p className={'text-gray-400 text-md'}>Aucune bande annonce</p> :
                 <div className={'flex flex-col gap-5'}>
                     <h4 className={'text-white font-bold mt-5 text-3xl mb-3'}>Bande Annonce</h4>
                     <iframe
-                        className={'aspect-video'}
+                        className={'aspect-video md:px-20'}
                         src={`https://www.youtube.com/embed/${videos.results[0].key}`}
                         title="YouTube video player"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
