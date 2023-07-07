67812f76f776707fd821c479c4a6e762dfec5d11
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-07 02:44:22 +0200

    feature: Création du composant de scroll de cartes d'acteurs.

---
+++ src/components/CastingScroller.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import CastingCard from "./CastingCard.jsx";
+
+export default function CastingScroller({ cast }) {
+    return (
+        <div className={'flex flex-col gap-5 w-full'}>
+            <h4 className={'text-white font-bold mt-5 text-3xl mb-3'}>Casting</h4>
+            <div className={'flex gap-7 castScroller'}>
+                {cast.cast.slice(0, 5).map((actor) => (
+                    <CastingCard actor={actor} />
+                ))}
+
+                <div className={'flex gap-4 items-center justify-center font-bold text-white text-xl'}>
+                    <span>Afficher plus</span>
+                    <FontAwesomeIcon icon={faChevronRight} className={'text-white'} />
+                </div>
+            </div>
+        </div>
+    );
+}
