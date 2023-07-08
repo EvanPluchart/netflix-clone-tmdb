1999e71635929626720a4d6bb6e6f66c7ccd0248
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-08 02:36:19 +0200

    feature: Création des fonction fetch pour les séries.

---
+++ src/services/api/tvshow.js
@@ -0,0 +1,38 @@
+import {API_KEY} from './movie';
+
+export function fetchTvshows(pageNumber) {
+    return fetch(
+        `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&page=${pageNumber}&language=fr-FR`
+    )
+        .then((response) => response.json())
+        .then((data) => data.results)
+        .catch((error) => console.error(error));
+}
+
+export function fetchTvshowDetails(showId) {
+    return fetch(`https://api.themoviedb.org/3/tv/${showId}?api_key=${API_KEY}&language=fr-FR`)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.error(error));
+}
+
+export function fetchTvshowCredits(showId) {
+    return fetch(`https://api.themoviedb.org/3/tv/${showId}/credits?api_key=${API_KEY}&language=fr-FR`)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.error(error));
+}
+
+export function fetchTvshowProviders(showId) {
+    return fetch(`https://api.themoviedb.org/3/tv/${showId}/watch/providers?api_key=${API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.error(error));
+}
+
+export function fetchTvshowVideos(showId) {
+    return fetch(`https://api.themoviedb.org/3/tv/${showId}/videos?api_key=${API_KEY}&language=fr-FR`)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => console.error(error));
+}
