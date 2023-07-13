d9b59c8a994ec4b431a504527d1d58ae42298218
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-13 15:00:50 +0200

    fix: Ajout d'une key au composant CastingCard dans le map.

--- src/components/CastingScroller.jsx
+++ src/components/CastingScroller.jsx
@@ -10,7 +10,7 @@ export default function CastingScroller({ cast, id }) {
             <h4 className={'text-white font-bold mt-5 text-3xl mb-3'}>Casting</h4>
             <div className={'flex gap-7 castScroller md:px-20 w-full'}>
                 {cast.cast.slice(0, 15).map((actor) => (
-                    <CastingCard actor={actor} />
+                    <CastingCard key={actor.name} actor={actor} />
                 ))}
 
                 <Link href={'/casting/' + id} className={'flex gap-4 items-center justify-center font-bold text-white text-xl'}>
