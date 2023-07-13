6083c3d85f0ffb24d09f2c53ce89f17d05f5d7ee
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-07-13 15:14:37 +0200

    feature: Mise en forme de la page d'erreur.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,10 +3,13 @@ import { Link } from "wouter";
 
 export default function Error() {
     return (
-        <div>
-            <h1>Erreur 404 : Page non trouvée</h1>
-            <p>Désolé, la page que vous recherchez n'a pas été trouvée.</p>
-            <Link href="/">Retour à l'accueil</Link>
+        <div className={'flex flex-col justify-center items-center text-center min-h-[100vh]'}>
+            <h1 className={'font-bold text-white text-4xl md:text-6xl mb-10'}>Erreur 404 : Page non trouvée</h1>
+            <p className={'font-bold text-white text-2xl'}>Désolé, la page que vous recherchez n'a pas été trouvée.</p>
+            <Link
+                href="/"
+                className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 mr-2 rounded my-5`}
+            >Retour à l'accueil</Link>
         </div>
     );
 }
